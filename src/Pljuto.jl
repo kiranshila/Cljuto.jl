af0d7910e7cbc87b1aa5ed2755604dbcb76f518e
Kiran Shila <me@kiranshila.com>
2021-09-20 14:35:31 -0700

    First Commit

--- src/Pljuto.jl
+++ src/Pljuto.jl
@@ -1,5 +1,20 @@
 module Pljuto
+using HypertextLiteral
 
-# Write your package code here.
+function cljs_bootstrap()
+    @htl("""
+    <script src="https://cdn.jsdelivr.net/gh/borkdude/scittle@0.0.2/js/scittle.js" type="application/javascript"></script>
+    <script crossorigin src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
+    <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/borkdude/scittle@0.0.2/js/scittle.reagent.js" type="application/javascript"></script>
+    <script> scittle.core.disable_auto_eval(true);</script>""")
+end
 
+macro cljs_str(cljs::String)
+    @htl("""<script>return scittle.core.eval_string($(cljs));</script>""")
+end
+
+# Assuming this was run from pluto, a call to bootstrap on module load should set it all up
+cljs_bootstrap()
+export @cljs,@cljs_str
 end
